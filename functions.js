404d55fc24c902e9779b6832eed82a881c0c1ed3
amandalynnelliott <aelliottart@gmail.com>
2016-12-19 22:09:25 -0500

    bare bones
    before adding any click events and testing which way of displaying
    frame-by-frame animation would work

---
+++ functions.js
@@ -0,0 +1,37 @@
+/*
+
+TODO
+
+FLOUR SACK STATES
+depressed, belly tickle, eye poke, drag up, drag right or left (?), falling, excited hello, ducks, kicks ball
+
+? Place img holder (only one image div with src that changes) in <canvas> drawImage and getImageData to not allow clicks on alpha 0 pixels
+
+EVENTS - FOR EACH, CHANGE IMG & ANIM NAME
+--- click event - belly location
+--- click event - eye location
+--- click and drag - draggable: true, or .ondrag
+------ let go, fall
+--- ? hover - eyes follow mouse - UNIT CIRCLE
+
+BUTTONS, ALSO CHANGE IMG & ANIM NAME
+--- timer display that makes animation "depressed" loop if runs out
+--- say hello (excited)
+--- throw ball (ducks)
+--- toss ball (kicks)
+
+UI - "Click on the flour sack!"
+Background horizon line image/ scene
+Animate and exchange placeholder images for GIFs, making sure that connection frames are the same. 
+--- might need to make some transition GIFs, ie: depressed to neutral
+
+*/
+
+
+// click belly
+
+// click eye
+
+// click and drag
+
+// unclick, fall
